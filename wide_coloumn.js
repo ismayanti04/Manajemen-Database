1f7bbbb2109dfb58e7492246a8f69e289a391b45
ismayanti04 <ismayantimamuju@gmail.com>
2025-06-20 16:08:14 +0800

    Create wide_coloumn.js

---
+++ wide_coloumn.js
@@ -0,0 +1,282 @@
+use projectUas
+switched to db projectUas
+db.createCollection("modelBarang");
+{ ok: 1 }
+db.modelBarang.insertMany([
+  {
+    kode: "BRG001",
+    nama: "Kulkas 2 Pintu",
+    merk: "Sharp",
+    stok: 10,
+    harga: 3500000
+  },
+  {
+    kode: "BRG002",
+    nama: "TV LED 40 Inch",
+    merk: "Samsung",
+    stok: 5,
+    harga: 4500000,
+    garansi: "2 Tahun"
+  },
+  {
+    kode: "BRG003",
+    nama: "Laptop Core i5",
+    merk: "Asus",
+    stok: 8,
+    harga: 7500000,
+    warna: "Hitam",
+    RAM: "8GB"
+  },
+  {
+    kode: "BRG004",
+    nama: "AC 1 PK",
+    merk: "Panasonic",
+    stok: 3,
+    harga: 5000000
+  },
+  {
+    kode: "BRG005",
+    nama: "Speaker Bluetooth",
+    merk: "JBL",
+    stok: 20,
+    harga: 950000,
+    warna: "Merah"
+  }
+])
+{
+  acknowledged: true,
+  insertedIds: {
+    '0': ObjectId('68550e6b4102f6d89350d396'),
+    '1': ObjectId('68550e6b4102f6d89350d397'),
+    '2': ObjectId('68550e6b4102f6d89350d398'),
+    '3': ObjectId('68550e6b4102f6d89350d399'),
+    '4': ObjectId('68550e6b4102f6d89350d39a')
+  }
+}
+db.modelBarang.find();
+{
+  _id: ObjectId('68550e6b4102f6d89350d396'),
+  kode: 'BRG001',
+  nama: 'Kulkas 2 Pintu',
+  merk: 'Sharp',
+  stok: 10,
+  harga: 3500000
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d397'),
+  kode: 'BRG002',
+  nama: 'TV LED 40 Inch',
+  merk: 'Samsung',
+  stok: 5,
+  harga: 4500000,
+  garansi: '2 Tahun'
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d398'),
+  kode: 'BRG003',
+  nama: 'Laptop Core i5',
+  merk: 'Asus',
+  stok: 8,
+  harga: 7500000,
+  warna: 'Hitam',
+  RAM: '8GB'
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d399'),
+  kode: 'BRG004',
+  nama: 'AC 1 PK',
+  merk: 'Panasonic',
+  stok: 3,
+  harga: 5000000
+}
+use projectUas
+switched to db projectUas
+db.createCollection("modelBarang");
+{ ok: 1 }
+db.modelBarang.insertMany([
+  {
+    kode: "BRG001",
+    nama: "Kulkas 2 Pintu",
+    merk: "Sharp",
+    stok: 10,
+    harga: 3500000
+  },
+  {
+    kode: "BRG002",
+    nama: "TV LED 40 Inch",
+    merk: "Samsung",
+    stok: 5,
+    harga: 4500000,
+    garansi: "2 Tahun"
+  },
+  {
+    kode: "BRG003",
+    nama: "Laptop Core i5",
+    merk: "Asus",
+    stok: 8,
+    harga: 7500000,
+    warna: "Hitam",
+    RAM: "8GB"
+  },
+  {
+    kode: "BRG004",
+    nama: "AC 1 PK",
+    merk: "Panasonic",
+    stok: 3,
+    harga: 5000000
+  },
+  {
+    kode: "BRG005",
+    nama: "Speaker Bluetooth",
+    merk: "JBL",
+    stok: 20,
+    harga: 950000,
+    warna: "Merah"
+  }
+])
+{
+  acknowledged: true,
+  insertedIds: {
+    '0': ObjectId('68550e6b4102f6d89350d396'),
+    '1': ObjectId('68550e6b4102f6d89350d397'),
+    '2': ObjectId('68550e6b4102f6d89350d398'),
+    '3': ObjectId('68550e6b4102f6d89350d399'),
+    '4': ObjectId('68550e6b4102f6d89350d39a')
+  }
+}
+db.modelBarang.find();
+{
+  _id: ObjectId('68550e6b4102f6d89350d396'),
+  kode: 'BRG001',
+  nama: 'Kulkas 2 Pintu',
+  merk: 'Sharp',
+  stok: 10,
+  harga: 3500000
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d397'),
+  kode: 'BRG002',
+  nama: 'TV LED 40 Inch',
+  merk: 'Samsung',
+  stok: 5,
+  harga: 4500000,
+  garansi: '2 Tahun'
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d398'),
+  kode: 'BRG003',
+  nama: 'Laptop Core i5',
+  merk: 'Asus',
+  stok: 8,
+  harga: 7500000,
+  warna: 'Hitam',
+  RAM: '8GB'
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d399'),
+  kode: 'BRG004',
+  nama: 'AC 1 PK',
+  merk: 'Panasonic',
+  stok: 3,
+  harga: 5000000
+}
+use projectUas
+switched to db projectUas
+db.createCollection("modelBarang");
+{ ok: 1 }
+db.modelBarang.insertMany([
+  {
+    kode: "BRG001",
+    nama: "Kulkas 2 Pintu",
+    merk: "Sharp",
+    stok: 10,
+    harga: 3500000
+  },
+  {
+    kode: "BRG002",
+    nama: "TV LED 40 Inch",
+    merk: "Samsung",
+    stok: 5,
+    harga: 4500000,
+    garansi: "2 Tahun"
+  },
+  {
+    kode: "BRG003",
+    nama: "Laptop Core i5",
+    merk: "Asus",
+    stok: 8,
+    harga: 7500000,
+    warna: "Hitam",
+    RAM: "8GB"
+  },
+  {
+    kode: "BRG004",
+    nama: "AC 1 PK",
+    merk: "Panasonic",
+    stok: 3,
+    harga: 5000000
+  },
+  {
+    kode: "BRG005",
+    nama: "Speaker Bluetooth",
+    merk: "JBL",
+    stok: 20,
+    harga: 950000,
+    warna: "Merah"
+  }
+])
+{
+  acknowledged: true,
+  insertedIds: {
+    '0': ObjectId('68550e6b4102f6d89350d396'),
+    '1': ObjectId('68550e6b4102f6d89350d397'),
+    '2': ObjectId('68550e6b4102f6d89350d398'),
+    '3': ObjectId('68550e6b4102f6d89350d399'),
+    '4': ObjectId('68550e6b4102f6d89350d39a')
+  }
+}
+db.modelBarang.find();
+{
+  _id: ObjectId('68550e6b4102f6d89350d396'),
+  kode: 'BRG001',
+  nama: 'Kulkas 2 Pintu',
+  merk: 'Sharp',
+  stok: 10,
+  harga: 3500000
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d397'),
+  kode: 'BRG002',
+  nama: 'TV LED 40 Inch',
+  merk: 'Samsung',
+  stok: 5,
+  harga: 4500000,
+  garansi: '2 Tahun'
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d398'),
+  kode: 'BRG003',
+  nama: 'Laptop Core i5',
+  merk: 'Asus',
+  stok: 8,
+  harga: 7500000,
+  warna: 'Hitam',
+  RAM: '8GB'
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d399'),
+  kode: 'BRG004',
+  nama: 'AC 1 PK',
+  merk: 'Panasonic',
+  stok: 3,
+  harga: 5000000
+}
+{
+  _id: ObjectId('68550e6b4102f6d89350d39a'),
+  kode: 'BRG005',
+  nama: 'Speaker Bluetooth',
+  merk: 'JBL',
+  stok: 20,
+  harga: 950000,
+  warna: 'Merah'
+}
